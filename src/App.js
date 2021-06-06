c3da823d52f4933f1ec8469c6e06e2ddbe96476a
Alec Scully <ScullyAlec@gmail.com>
2021-06-06 12:49:20 -0400

    Moved contact to contact component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 // To push to the live site run:
 // npm run deploy
 
-
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar'
@@ -40,36 +38,6 @@ function App() {
         </Route>
 
       </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Portfolio under construction! More coming soon!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Alec-Scully"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/alec-scully/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-          className="App-link"
-          href="https://scully-alec.medium.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Blog
-        </a>
-      </header>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import logo from '../logo.svg';
 
 class Contact extends Component {
 
@@ -7,6 +8,36 @@ class Contact extends Component {
         return (
             <div>
                 This is my Contact component
+                <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Portfolio under construction! More coming soon!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Alec-Scully"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/alec-scully/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://scully-alec.medium.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Blog
+        </a>
+      </header>
             </div>
         )
     }
